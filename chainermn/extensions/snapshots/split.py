d096569654de79745c71d04b38496e89050353ac
Yohei Tsuji <t.t.t.yohei@gmail.com>
2017-09-13 12:18:35 +0900

    Fixed typo

--- chainermn/extensions/snapshots/split.py
+++ chainermn/extensions/snapshots/split.py
@@ -4,17 +4,18 @@ from pympler import asizeof
 
 from chainermn.extensions.snapshots import util
 
-"""THIS FILE IS NOT COMPLETED!
-"""
-
-
 def split(comm):
+    """
+    ONLY NPZ FORMAT IS AVAILABLE FOR THIS FUNCTION
+    """
 
     def split(filename, outdir, handler):
         target = handler.target
         participants = [x for x in range(0, comm.size)]
+        srank = participants.index(comm.rank)
         root = participants[0]
         memsizes = {}
+        local_target = {}
         pattern = 'updater/'
         # items() is defined in both npz and hdf5 format.
         for k, v in target.items():
@@ -24,7 +25,7 @@ def split(comm):
             else:
                 if srank == root:
                     local_target[k] = v
-        memsizes = collections.OrderdDict(sorted(memsizes.items(),
+        memsizes = collections.OrderedDict(sorted(memsizes.items(),
                                                  key=lambda x: x[1]))
         for i in range(0, len(memsizes)):
             k, v = memsizes.popitem()
@@ -32,6 +33,7 @@ def split(comm):
                 local_target[k] = target[k]
 
         # Overwrite original target
-        target = local_target
-        filename = filename + '_{}'.format()
+        handler.target = local_target
         util.snapshot(filename, outdir, handler)
+
+    return split
